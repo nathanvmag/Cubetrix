33ed04ad977ef07f2774b8e4c73073db087c6f79
Nathan Vieira <nathanvmag@gmail.com>
2016-04-04 16:14:42 -0300

    primeiro comit

---
+++ teste 3(trab jamv c cris)/Assets/Scripts/Novo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Novo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("left")) 
+		{	
+			transform.Rotate(0,90,0,Space.World);
+		}
+
+		if (Input.GetKeyDown ("right")) 
+		{
+			transform.Rotate(0,90,0,Space.World);
+		}
+
+		if (Input.GetKeyDown ("up")) 
+		{
+			transform.Rotate(0,0,-90,Space.World);
+		}
+
+		if (Input.GetKeyDown ("down")) 
+		{
+			transform.Rotate(0,0,90,Space.World);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+
+	}
+}
---
+++ teste 3(trab jamv c cris)/Assets/Scripts/Plano.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plano  : MonoBehaviour {
+    static public float speed = 2;
+    public GameObject alvo;
+    public GameObject[] peças,jogadores;
+    public static GameObject ObjComparar;
+    public static GameObject player; 
+    public GameObject localplayer; 
+    
+	static public int controle = 0 ; 
+	public GameObject nextPlane ;
+   
+    private int[] angles = {0,90,180,270};
+   
+	// Use this for initialization
+	void Start () {
+        
+        alvo.gameObject.name = "ponto";
+		player = null; 
+       
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, alvo.transform.position, speed*Time.deltaTime);
+		if (localplayer.transform.position.x == transform.position.x) {
+
+			if (player!=null && ObjComparar!=null)
+			{	
+				if (player.transform.rotation == ObjComparar.transform.rotation) Debug.Log("vai virar jogo");
+				else Debug.Log("tu é ruim");
+			}
+			if (ObjComparar!= null) Destroy(ObjComparar.gameObject);
+			
+			transform.position = new Vector3(65,transform.position.y,transform.position.z);
+			speed = speed == 10f? speed = 10f : speed+0.2f;
+			
+			if(transform.childCount > 0)
+			{
+				foreach(Transform t in transform)
+				{
+					Destroy(t.gameObject);
+				}
+			} 
+			newbloco();
+		
+		}
+	}
+     void OnTriggerEnter(Collider coll)
+    {
+       
+    }
+	void newbloco()
+	{
+		if (player != null) Destroy(player.gameObject);
+		if (controle == 3) {
+			controle = 0;
+		} else {
+			controle ++;
+		}
+
+		string objectname = "Plane" + controle;
+		//Debug.Log (objectname);
+		nextPlane = GameObject.Find (objectname);
+		int rand = Random.Range(0, peças.Length);
+		player = Instantiate(jogadores[rand], localplayer.transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
+
+		ObjComparar = Instantiate(peças[rand],nextPlane.transform.position, Quaternion.Euler(new Vector3(0, angles[Random.Range(0, angles.Length)], angles[Random.Range(0, angles.Length)]))) as GameObject;
+		ObjComparar.transform.parent = nextPlane.transform;
+		ObjComparar.transform.position += new Vector3(-2.4f, 2, 0);
+		//Debug.Log (speed);
+	}	
+}
