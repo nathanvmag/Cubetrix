c9f6df16337a647c5246ea76b878dc9fe827debe
Nathan Vieira <nathanvmag@gmail.com>
2016-06-27 21:50:20 -0300

    1.6.2

--- teste 3(trab jamv c cris)/Assets/Scripts/Name.cs
+++ teste 3(trab jamv c cris)/Assets/Scripts/Name.cs
@@ -10,7 +10,7 @@ public class Name : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-        PlayerPrefs.DeleteAll();
+        
         serviceapi = new ServiceAPI("fc1ab1a6bc1505b184f09e1fbe499a61edec16818ea13d07cdb85482b19d6342", "ad67abfb73dcd7ec24e353d705fb0a1212900958f2b79682ffec2d7b68bc2384");
        
         if (PlayerPrefs.GetString("nome") == null||PlayerPrefs.GetString("nome") == "" )
