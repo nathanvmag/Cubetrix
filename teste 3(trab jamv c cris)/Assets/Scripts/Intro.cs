678cdbae1dd08dfe1a4e4034fa006a13ad4a7b9b
Nathan Vieira <nathanvmag@gmail.com>
2016-11-30 20:50:32 -0200

    Bugfix

--- teste 3(trab jamv c cris)/Assets/Scripts/Intro.cs
+++ teste 3(trab jamv c cris)/Assets/Scripts/Intro.cs
@@ -7,9 +7,9 @@ public class Intro : MonoBehaviour {
 	void Start () {
 		if (Screen.currentResolution.width > 800 && Screen.currentResolution.height > 480&&( Application.platform.Equals(RuntimePlatform.Android) || Application.platform.Equals(RuntimePlatform.IPhonePlayer))) {
 			Screen.SetResolution (800, 480, true);
-
+			Handheld.PlayFullScreenMovie ("intro.mp4", Color.white, FullScreenMovieControlMode.CancelOnInput,FullScreenMovieScalingMode.Fill);
 		}	
-		Handheld.PlayFullScreenMovie ("intro.mp4", Color.white, FullScreenMovieControlMode.CancelOnInput,FullScreenMovieScalingMode.Fill);
+
 
 	}
 	
